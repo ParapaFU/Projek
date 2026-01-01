bcc19162c73d2c01b96746e9137961a6daafa4b0
Parapa <parapasndy@gmail.com>
2026-01-01 19:52:05 +0700

    add function toggle detail

---
+++ Portofolio/main.js
@@ -0,0 +1,14 @@
+function toggleDetail(e) {
+    const target = $(e.target);
+
+    if($(target).hasClass('active')) {
+        $(target).text('More Info').removeClass('active');
+    } else {
+        $(target).text('Less Info').addClass('active');
+    }
+    
+    const item = $(target).parents('.about-exp-item');
+    const detail = $(item).children(".about-exp-item-detail");
+
+    detail.slideToggle();
+}
